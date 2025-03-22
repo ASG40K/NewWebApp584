1d765490dee147c8d21c31c1ecf6edfa71864be1
ASG40K <anthonygalindo185@gmail.com>
2025-03-22 16:32:44 -0700

    New Updates (Still need to figure a theme)

--- newwebapp.client/src/proxy.conf.js
+++ newwebapp.client/src/proxy.conf.js
@@ -1,14 +1,11 @@
-const { env } = require('process');
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7036';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      '/weatherforecast',
     ],
-    target,
+    target: "https://localhost:7036",
+
     secure: false
   }
 ]
